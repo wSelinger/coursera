da8aa2d39d7824dd57911574ded35bcf80684551
Wolfgang <Wolfgang>
2016-11-25 23:26:21 +0100

    initial

---
+++ fs05-module1-solution/app.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+angular.module("LunchCheck", [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.checkIfTooMuch = function() {
+    var nrOfDishes = countDishes($scope.dishes);
+
+    $scope.message = chooseMessage(nrOfDishes);
+    $scope.validation = validateInput(nrOfDishes);
+  };
+}
+
+function countDishes(dishesInput) {
+  if (!dishesInput)
+    return 0;
+
+  var dishes = dishesInput.split(",");
+  var nrOfDishes = 0;
+  for (var i=0; i < dishes.length; i++) {
+    if (dishes[i].trim()) {
+      nrOfDishes++;
+    }
+  }
+  return nrOfDishes;
+}
+
+function chooseMessage(nrOfDishes) {
+  if (nrOfDishes == 0)
+    return "Please enter data first";
+  else if (nrOfDishes <= 3)
+      return "Enjoy!";
+  else
+      return "Too much!";
+}
+
+function validateInput(nrOfDishes) {
+  return nrOfDishes == 0 ? "inputError" : "inputOk";
+}
+
+})();
